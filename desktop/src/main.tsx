022f0be18494e531585ac990a827cc549fe61df2
Prabhani Jeewanthika <iit22049@std.uwu.ac.lk>
2026-09-16 10:53:24 +0530

    feat: initialize project with backend, desktop, and mobile application components

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './i18n.ts' // Import i18n FIRST
 import App from './App.tsx'
-import './i18n.ts'
 import { AuthProvider } from './context/AuthContext';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-
+    <I18nextProvider i18n={i18n}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </I18nextProvider>
   </StrictMode>,
 )
